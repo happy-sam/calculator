25b401c0ae459bd4341bf947631807c9fba80dc1
Stanisław Romek <info@happy.ovh>
2018-07-22 14:56:23 +0200

    Dodanie projektu

---
+++ js/script.js
@@ -0,0 +1,130 @@
+$(document).ready(function(){
+    
+    
+    $("input[name='on']").click(function(){        
+        $("input[name='screen']").val("");        
+    });
+    
+    $("input[name='sum']").click(function(){        
+        var suma = eval(calculator.screen.value);
+        $("input[name='screen']").val(suma);
+    });
+    
+    $("input[name='element']").click(function(){        
+        $("input[name='screen']").val();        
+    });
+        
+    
+    document.addEventListener('keyup', function(e){
+        var liczba = e.key;         
+        
+        switch(liczba){
+            case "1": {
+                console.log("1");
+                $("input[name='screen']").val("1");        
+                break;
+            }
+            case "2": {                
+                $("input[name='screen']").val("2");        
+                break;
+            }
+            case "3": {                
+                $("input[name='screen']").val("3");        
+                break;
+            }
+            case "4": {                
+                $("input[name='screen']").val("4");        
+                break;
+            }
+            case "5": {                
+                $("input[name='screen']").val("5");        
+                break;
+            }
+            case "6": {                
+                $("input[name='screen']").val("6");        
+                break;
+            }
+            case "7": {                
+                $("input[name='screen']").val("7");        
+                break;
+            }
+            case "8": {                
+                $("input[name='screen']").val("8");        
+                break;
+            }
+            case "9": {                
+                $("input[name='screen']").val("9");        
+                break;
+            }
+            case "0": {                
+                $("input[name='screen']").val("0");        
+                break;
+            }
+            case "+": {                
+                $("input[name='screen']").val("+");        
+                break;
+            }
+            case "-": {                
+                $("input[name='screen']").val("-");        
+                break;
+            }
+            case "/": {                
+                $("input[name='screen']").val("/");        
+                break;
+            }
+            default: {
+                
+            }
+        }
+    });
+    
+    
+//    document.addEventListener('keyup', function(e) {
+//        switch(e.key){
+//            case 1:
+//                $("input[name='screen']").val(e.key);        
+//
+//                break;
+//            
+//        }
+//        
+        
+    
+
+//    var a = false;
+//    var b = false;
+//    function clearScreen() {
+//
+//        if (a) {
+//            calc.screen.value = ''
+//            a= false;
+//        } else {
+//
+//            calc.screen.value = eval(calc.screen.value);
+//            a = true;
+//            b = true;  
+//        }
+//
+//    }
+
+//    function keyReset(value) {
+//        if (!b) {
+//            calc.screen.value += value;
+//        } else {
+//            if (typeof value == 'number') {
+//                calc.screen.value = '';
+//            }
+//            calc.screen.value += value;
+//            b = false;
+//            a = false;
+//        }
+//    } 
+        
+});
+
+//function sum(){
+//    calc.screen.value = eval(calc.screen.value);
+//}
+//              
+
+        
